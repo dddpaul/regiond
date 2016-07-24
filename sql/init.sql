b7c1991bccdcaedcca1a638f3fc95f9d9c6bb701
Pavel Derendyaev <paul@reic.ru>
2016-07-24 12:02:28 +0300

    Add Oracle container, bump to 1.2

---
+++ sql/init.sql
@@ -0,0 +1,24 @@
+CREATE TABLE ip_to_region (
+  ip    varchar2(20) NOT NULL
+  ,region    number(10) NOT NULL
+);
+
+INSERT INTO ip_to_region (ip, region) VALUES ('127.0.0.1', 1);
+INSERT INTO ip_to_region (ip, region) VALUES ('10.0.0.1', 1);
+INSERT INTO ip_to_region (ip, region) VALUES ('10.0.0.2', 1);
+INSERT INTO ip_to_region (ip, region) VALUES ('10.0.0.3', 1);
+INSERT INTO ip_to_region (ip, region) VALUES ('10.0.0.4', 1);
+INSERT INTO ip_to_region (ip, region) VALUES ('10.0.0.5', 1);
+INSERT INTO ip_to_region (ip, region) VALUES ('10.0.0.6', 1);
+INSERT INTO ip_to_region (ip, region) VALUES ('10.0.0.7', 1);
+INSERT INTO ip_to_region (ip, region) VALUES ('10.0.0.8', 1);
+INSERT INTO ip_to_region (ip, region) VALUES ('10.0.0.9', 1);
+INSERT INTO ip_to_region (ip, region) VALUES ('20.0.0.1', 2);
+INSERT INTO ip_to_region (ip, region) VALUES ('20.0.0.2', 2);
+INSERT INTO ip_to_region (ip, region) VALUES ('20.0.0.3', 2);
+INSERT INTO ip_to_region (ip, region) VALUES ('20.0.0.4', 2);
+INSERT INTO ip_to_region (ip, region) VALUES ('20.0.0.5', 2);
+INSERT INTO ip_to_region (ip, region) VALUES ('20.0.0.6', 2);
+INSERT INTO ip_to_region (ip, region) VALUES ('20.0.0.7', 2);
+INSERT INTO ip_to_region (ip, region) VALUES ('20.0.0.8', 2);
+INSERT INTO ip_to_region (ip, region) VALUES ('20.0.0.9', 2);
---
+++ sqlplus.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sqlplus system/oracle@localhost/xe
